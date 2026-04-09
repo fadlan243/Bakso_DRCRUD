20793972aa77ab7fe1ba36d9e5231ffe9f577710
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-09 18:51:58 +0700

    event tombol koneksi

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -41,4 +41,25 @@ namespace CRUDMahasiswaADO
             }
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase();
+        }
+
         
